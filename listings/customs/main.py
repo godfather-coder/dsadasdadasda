eafe0aee2fd68c66c84b831de4c1aabf75e49f3a
Papuna Tsereteli <117572811+seteri@users.noreply.github.com>
2024-06-06 14:38:41 +0400

    Update main.py

--- listings/customs/main.py
+++ listings/customs/main.py
@@ -1,18 +1,35 @@
 from .getListingData import UrlFetcher
 from .formatData import FormatData
 from .uploadListing import AddListing
+import base64
+import json
 
+
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ2IjoiMSIsImlhdCI6MTcxNzUyNTk1NCwiZXhwaXJlc19hdCI6MTcxNzUyNjYxNCwiZGF0YSI6eyJ1c2VyX2lkIjo1MzY0NTEzLCJ1c2VybmFtZSI6InRhcmllbGludmVzdEBnbWFpbC5jb20iLCJzZXNzaW9uX2lkIjoiM2ZhN2Y1YzIxZGU0NmJkMmU3NzY0ZGU3ODA4YjgwNGNiNjNlZjIyMDQzZmYwNWZlODM4NjAyZmUzYjRmNmY2MmFmY2UyY2EzNmQ4ZjgyNWNkZWFkM2E1MDhjZGQ4YTI1MTliNmVlYzI2NmY4MWY0MzVmYTUzM2FmMGRiYzI4ZGMiLCJsaXZvX3VzZXJfaWQiOjEwMTY4MDIxLCJ2ZW5kb29fdXNlcl9pZCI6bnVsbCwic3dvb3BfdXNlcl9pZCI6bnVsbCwiZ2VuZGVyX2lkIjoxLCJiaXJ0aF95ZWFyIjoxOTk4LCJiaXJ0aF9kYXRlIjpudWxsLCJwaG9uZSI6IjU5MTA4MjQ5MyIsInVzZXJfbmFtZSI6IlRhcmllbCIsInVzZXJfc3VybmFtZSI6IkludmVzdCIsInR5cGVfaWQiOjB9fQ.r3UflsADC-O9hHSAS3TQkui-aznVRWcW7vf9nFU4BvAOoDtkxJrpv8cuCRo4G6zev7diqb-whCkpAWhuRoG22oWxdvdVsn-dpRDrIdTaEZQV4Rlt9CEHYSFe1V4w6jnWhYE6U4uaKvM5BxJNCmHO5v40zzsQnFpKyvIUAQekntdzjFWehEhqOQBEZp0ipENtZCecowOGNFY9JnD5qXOP1tndRMjBWS8_kWuIwppYhPn5OMeNmR88RKLcqFgnYG0SihjdxMKJvaas-2k0CDNhMrVk05huTJwx7HIrhfkdXs2DpbGWy4t4xgvI9GH8jen-gZCOi6VnhI2WD3Cd_UVLFQ"
+
+header, payload, signature = token.split(".")
+
+decoded_payload = base64.urlsafe_b64decode(payload + '=' * (-len(payload) % 4))
+decoded_payload_json = json.loads(decoded_payload)
+
+print(decoded_payload_json.get('data').get('phone'))
+print(decoded_payload_json.get('data').get('user_name'))
+
+exit(200)
 test = UrlFetcher()
+
+data = test.fetch_data(18266826)
+
 convert = FormatData()
+
+convertedData = convert.convert_to_upload_format(
+    data, decoded_payload_json.get('data').get('phone')
+    , decoded_payload_json.get('data').get('user_name')
+)
+
 uploader = AddListing()
 
-
-def upload(ids, token):
-    for singleId in ids:
-        data = test.fetch_data(singleId)
-
-        convertedData = convert.convert_to_upload_format(data, '568625279')
-
-        uploader.setHeaders(token)
-
-        print(uploader.data(convertedData))
+# token = ("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9"
+#          ".eyJ2IjoiMSIsImlhdCI6MTcxNzAyNDYxNSwiZXhwaXJlc19hdCI6MTcxNzAyNTI3NSwiZGF0YSI6eyJ1c2VyX2lkIjo1MzY0NTEzLCJ1c2VybmFtZSI6InRhcmllbGludmVzdEBnbWFpbC5jb20iLCJzZXNzaW9uX2lkIjoiYWJhNDU4YmU0YzkzYTA1ODE3NWQ2ODg3Y2Q2NjE5OThjYTVkYzAwZDI1MGY1NWVkNGViNmEyNjg4NWZhZjBhOWFhMTIxYzg4OTY5ZTI3ZmIyOWFiZDExN2Q3ZGM2MmYxNTJhYmQyNjcxZTQ5NWY4OTIwZDcxYmFlZTFmMzEwMDMiLCJsaXZvX3VzZXJfaWQiOjEwMTY4MDIxLCJ2ZW5kb29fdXNlcl9pZCI6bnVsbCwic3dvb3BfdXNlcl9pZCI6bnVsbCwiZ2VuZGVyX2lkIjoxLCJiaXJ0aF95ZWFyIjoxOTk4LCJiaXJ0aF9kYXRlIjpudWxsLCJwaG9uZSI6IjU5MTA4MjQ5MyIsInVzZXJfbmFtZSI6IlRhcmllbCIsInVzZXJfc3VybmFtZSI6IkludmVzdCIsInR5cGVfaWQiOjB9fQ.W77T4co19qaCg8zLt4gnvgRKJ8yL591aWzwVHDf5mWnKC1rot7ai5Kk3tKU_ORPHp3BYbGzUL3gXPCI2laqFVtOfxcqKtMcan-5dz2-dQjLyxSRw4od0b1SKTnoLv9JNjGon69K24iad1l6g5ygDhxTe-MPmY2kJt3Q7UXvxLBRZ74FhQISbSZVZ3tn6pZt-__poLZENRsnn0tbQX4FbZgl7bLjt5182v3ud_XW5R6i-dFPYd0AvQNOoe1FzzrkwOPdSbbw8DEIDDjTY-_gfU7wTNhVFwvm_0oHvsPvMoCFJ04-SIYfQqFp7GSjmkomHOlAiQDXXM2jP_iAJVw3zfg")
+uploader.setHeaders(token)
+print(uploader.data(convertedData))
